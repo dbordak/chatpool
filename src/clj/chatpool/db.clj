9dea216d4892515311f3bfb6cd633a9af268ccb1
Daniel Bordak <dbordak@fastmail.fm>
2016-12-19 10:20:25 -0500

    Fix DATABASE_URL

--- src/clj/chatpool/db.clj
+++ src/clj/chatpool/db.clj
@@ -3,10 +3,10 @@
 
 ;; This file pretty much just exists to namespace the SQL functions.
 
-(def db {:classname "org.postgresql.Driver"
-         :subprotocol "postgresql"
-         :subname (or (System/getenv "DATABASE_URL")
-                      "//localhost:5432/chatpool")})
+(def db (or (System/getenv "DATABASE_URL")
+            {:classname "org.postgresql.Driver"
+             :subprotocol "postgresql"
+             :subname "//localhost:5432/chatpool"}))
 
 (defqueries "sql/reps.sql" {:connection db})
 (defqueries "sql/chatpool.sql" {:connection db})
